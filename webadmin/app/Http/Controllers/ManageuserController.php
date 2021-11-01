275747c8778a5aaafdcc0be58aa7dce648b56044
Raghunath-kumar <93525146+Raghunath-kumar@users.noreply.github.com>
2021-11-01 17:46:40 +0530

    1 change

---
+++ webadmin/app/Http/Controllers/ManageuserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManageuserController extends Controller
+{
+    public function managefunction()
+    {
+        return view('manageuser');
+    }
+}
