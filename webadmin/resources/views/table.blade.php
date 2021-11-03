19f218de6412b369a825347d603552104c80407f
Raghunath-kumar <93525146+Raghunath-kumar@users.noreply.github.com>
2021-11-03 15:16:35 +0530

    table page update

---
+++ webadmin/resources/views/table.blade.php
@@ -0,0 +1,33 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <div class="container">
+      <center><h1 style="color:blue;text-align:center;">USER TABLE</h1></center>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>User ID</th>
+            <th>Role</th>
+            <th>Name</th>
+            <th>Mobile</th>
+            <th>Email</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Password</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>101</td>
+            <td>Project Manager</td>
+            <td>Raj</td>
+            <td>8709216859</td>
+            <td>eshuzo.raj@gmail.com</td>
+            <td>Ranchi</td>
+            <td>Jharkhand</td>
+            <td>123456789</td>
+            <td>50% done</td>
+
+
+
+
